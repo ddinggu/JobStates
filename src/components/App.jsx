9812cbbcb9dd659fd4d37583f15feb233094acb5
donguk <dongukcha0@gmail.com>
2018-11-16 13:16:22 +0900

    onging header

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
 
+import Header from './header';
+
 class App extends Component {
   render() {
-    return <div className="App">Job states</div>;
+    return(
+      <div className="App">
+        <Header />
+      </div>
+    ) 
   }
 }
 
