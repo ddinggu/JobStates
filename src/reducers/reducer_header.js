cd947b04019cf4b727718aa31f3c7d0c6bd10d43
donguk <dongukcha0@gmail.com>
2018-11-19 11:26:46 +0900

    complete header

--- src/reducers/reducer_header.js
+++ src/reducers/reducer_header.js
@@ -3,6 +3,7 @@ import { FETCH_HEADER } from '../actions/action_header';
 const initialState = [];
 
 export default function(state = initialState, action) {
+  console.log('action:::', action)
   switch (action.type) {
     case FETCH_HEADER:
       return [action.payload, ...state];
