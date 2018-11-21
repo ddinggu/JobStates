94321bae94abd944f0224918385be34a2b77f773
yyijoo <yyijoo@gmail.com>
2018-11-21 17:11:30 +0900

    merged upstream dev with userprofile updated version

--- src/index.js
+++ src/index.js
@@ -4,10 +4,6 @@ import { Provider } from 'react-redux';
 import App from './components/App';
 import store from './store';
 import 'semantic-ui-css/semantic.min.css';
-<<<<<<< HEAD
-
-=======
->>>>>>> 9737abcbbe0a690c536a27c6b3b316502f1b2edd
 // import * as serviceWorker from './serviceWorker';
 
 // import { fetchUserProfile } from './actions';
