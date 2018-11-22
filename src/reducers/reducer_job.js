3d63cf02734575fb7dac58383fdd3c56d48d0e3a
donguk <dongukcha0@gmail.com>
2018-11-22 10:56:36 +0900

    bug fix!

--- src/reducers/reducer_job.js
+++ src/reducers/reducer_job.js
@@ -1,21 +1,22 @@
-import { FETCH_JOB } from '../actions/action_Job';
-import { SEARCH_FILTER } from '../actions/action_Job';
+import { FETCH_JOB, SEARCH_FILTER } from '../actions/action_Job';
 
 const initialState = {
   allJobData: [],
   filterData: [],
 };
-// const initialState = [];
 
 export default function (state = initialState, action) {
-  // console.log('job action::: ', action);
+  console.log('job action::: ', action);
   switch (action.type) {
     case FETCH_JOB:
-      console.log('in reducer', action.type, action.payload);
-      console.log(initialState.allJobData);
       return {
         ...state,
         allJobData: action.payload,
+      };
+    case SEARCH_FILTER:
+      return { 
+        ...state,
+        filterData: action.payload,
       };
     default:
       return state;
