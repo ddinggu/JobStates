0497f0329d862f48532d94c336d786b27e09e329
ddinggu <travelrudwls@gmail.com>
2018-11-24 16:15:36 +0900

    temp to bugfix

--- src/actions/action_Job.js
+++ src/actions/action_Job.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import * as types from 'actions/actionTypes';
+import * as api from 'api/api';
 
 const URL_JOB = 'http://ec2-54-218-47-139.us-west-2.compute.amazonaws.com/job';
 
@@ -7,13 +8,46 @@ export const FETCH_JOB = 'FETCH_JOB';
 export const SEARCH_FILTER = 'SEARCH_FILTER';
 
 export const fetchJob = () => dispatch => axios.get(URL_JOB).then((res) => {
-  dispatch({ type: FETCH_JOB, payload: res.data });
-});
+    dispatch({ type: FETCH_JOB, payload: res.data });
+  });
 
-export const filterFetchData = (filterTargetValue, filterTargetInputValue) => (dispatch) => {
-  dispatch({ type: SEARCH_FILTER, payload: filterTargetValue, payload2: filterTargetInputValue });
+export const filterFetchData = (
+  filterTargetValue,
+  filterTargetInputValue,
+) => (dispatch) => {
+  dispatch({
+    type: SEARCH_FILTER,
+    payload: filterTargetValue,
+    payload2: filterTargetInputValue,
+  });
 };
 
 export const getDetailJob = id => (dispatch) => {
   dispatch({ type: types.GET_DETAIL_JOB, id });
+};
+
+const loadingDeleteJobData = () => ({
+  type: types.DELETE_JOB_BEGIN,
+});
+
+const successDeleteJobData = hireId => ({
+  type: types.DELETE_JOB_SUCCESS,
+  hireId,
+});
+
+const failedDeleteJobData = error => ({
+  type: types.DELETE_JOB_FAILURE,
+  error,
+});
+
+export const deleteJobData = hireId => async (dispatch) => {
+  dispatch(loadingDeleteJobData());
+
+  try {
+    const response = await api.deletePostingJob(hireId);
+    dispatch(successDeleteJobData(response.data));
+  } catch (err) {
+    console.error(err);
+    dispatch(failedDeleteJobData(err));
+  }
 };
--- src/containers/job/JobDetailHeader.js
+++ src/containers/job/JobDetailHeader.js
@@ -1,11 +1,53 @@
 import React, { Component } from 'react';
+import { Button, Icon, Modal } from 'semantic-ui-react';
 
 class JobDetailHeader extends Component {
+  state = { open: false };
+
+  onModifyClick = () => {};
+
+  onDeleteClick = () => {};
+
+  show = () => this.setState({ open: true });
+  close = () => this.setState({ open: false });
+
   render() {
+    const { open } = this.state;
+
     return (
-      <div>
-        <input type="submit" />
-        <input type="submit" />
+      <div style={{ float: 'right' }}>
+        <Button icon labelPosition="left" style={{ marginRight: '2rem' }}>
+          <Icon name="cut" />
+          수정
+        </Button>
+        <Button
+          icon
+          labelPosition="left"
+          style={{ marginRight: '4rem' }}
+          onClick={this.show}
+        >
+          <Icon name="eraser" />
+          삭제
+        </Button>
+
+        <Modal size={'mini'} open={open} onClose={this.close}>
+          <Modal.Header>삭제</Modal.Header>
+          <Modal.Content>
+            <p>정말 삭제하시겠습니까?</p>
+          </Modal.Content>
+          <Modal.Actions>
+            <Button negative onClick={this.close}>
+              취소
+            </Button>
+            <Button
+              positive
+              icon="checkmark"
+              labelPosition="right"
+              content="삭제"
+              onClick={this.close}
+            />
+          </Modal.Actions>
+        </Modal>
       </div>
     );
   }
