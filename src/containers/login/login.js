b10c782b55930f53bd7233ca783f228e049a28f3
yyijoo <yyijoo@gmail.com>
2018-12-03 20:01:52 +0900

    Apply Styled Component: Login

--- src/containers/login/login.js
+++ src/containers/login/login.js
@@ -1,46 +1,55 @@
 import React, { Component } from 'react';
-import { Container, Button, Grid } from 'semantic-ui-react';
 import * as api from 'api/api';
 import styled from 'styled-components';
 
+// ============= Styled Components ============= //
 const Button_st = styled.button`
   border-radius: 50px;
   border: none;
   height: 35px;
-  margin: auto;
-  color: white;
-  width: 50%;
+  width: 300px;
+  color: black;
   background-color: ${props => (props.google ? '#dd4b39;' : props.kakao ? '#ffeb04;' : '#03c73c;')};
+  display: inline-block;
 `;
 const Anchor = styled(Button_st.withComponent('a'))``;
+
+const Wrapper = styled.div`
+  display: grid;
+  grid-template-rows: 50px 35px 35px 35px;
+  grid-gap: 5px;
+`;
+
+const Box = styled.div`
+  display: inline-block;
+  text-align: center;
+  line-height: 35px;
+  grid-column: ${props => props.column};
+  grid-row: ${props => props.row};
+`;
+
+// ============================================= //
 
 export default class Login extends Component {
   render() {
     return (
-      <Container>
-        <Grid centered>
-          <Grid.Row verticalAlign="middle" />
-          <Grid.Row>
-            <Grid.Column />
-            <Grid.Column width={7}>
-              <Grid padded>
-                <Anchor google href={`${api.URL}/auth/google`}>
-                  구글로 로그인
-                </Anchor>
-                <Grid.Row>
-                  <Button_st github href={`${api.URL}/auth/github`}>
-                    깃헙으로 로그인
-                  </Button_st>
-                </Grid.Row>
-                <Button_st kakao href={`${api.URL}/auth/kakao`}>
-                  카카오로 로그인
-                </Button_st>
-              </Grid>
-            </Grid.Column>
-            <Grid.Column />
-          </Grid.Row>
-        </Grid>
-      </Container>
+      <Wrapper>
+        <Box column="1/2" row="2/3">
+          <Anchor google href={`${api.URL}/auth/google`}>
+            구글로 로그인
+          </Anchor>
+        </Box>
+        <Box column="1/2" row="3/4">
+          <Anchor github href={`${api.URL}/auth/github`}>
+            깃헙으로 로그인
+          </Anchor>
+        </Box>
+        <Box column="1/2" row="4/5">
+          <Anchor kakao href={`${api.URL}/auth/kakao`}>
+            카카오로 로그인
+          </Anchor>
+        </Box>
+      </Wrapper>
     );
   }
 }
